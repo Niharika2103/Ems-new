6d7b937e11f06adea5cc065317be6ea02ec144a2
VanisriMurugan-31 <vanisree.m@zigmaneural.com>
2025-10-03 20:54:39 +0530

    in master set utils localhost

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,15 +1,15 @@
 // Different base URLs per role
-// export const API_BASES = {
-//   SUPERADMIN: import.meta.env.VITE_SUPERADMIN_URL || "http://localhost:5001",
-//   ADMIN: import.meta.env.VITE_ADMIN_URL || "http://localhost:5002",
-//   EMPLOYEE: import.meta.env.VITE_EMPLOYEE_URL || "http://localhost:5004",
-// };
-
 export const API_BASES = {
-  SUPERADMIN: import.meta.env.VITE_SUPERADMIN_URL || "https://auth-service-nuob.onrender.com",
-  ADMIN: import.meta.env.VITE_ADMIN_URL || "https://admin-service-vb0h.onrender.com",
-  EMPLOYEE: import.meta.env.VITE_EMPLOYEE_URL || "https://employee-service-5od6.onrender.com",
+  SUPERADMIN: import.meta.env.VITE_SUPERADMIN_URL || "http://localhost:5001",
+  ADMIN: import.meta.env.VITE_ADMIN_URL || "http://localhost:5002",
+  EMPLOYEE: import.meta.env.VITE_EMPLOYEE_URL || "http://localhost:5004",
 };
+
+// export const API_BASES = {
+//   SUPERADMIN: import.meta.env.VITE_SUPERADMIN_URL || "https://auth-service-nuob.onrender.com",
+//   ADMIN: import.meta.env.VITE_ADMIN_URL || "https://admin-service-vb0h.onrender.com",
+//   EMPLOYEE: import.meta.env.VITE_EMPLOYEE_URL || "https://employee-service-5od6.onrender.com",
+// };
 
 
 // Auth endpoints
