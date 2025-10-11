48db8a01adf3118533f44dae744ff0075cff9776
KishoreIndia-81 <kishore.c@artificalintelligenceindia.in>
2025-10-11 12:33:39 +0530

    Save all updates for attendance-service module

---
+++ backend-java/attendance-service/src/main/java/com/example/attendance_service/model/ProjectEntity.java
@@ -0,0 +1,49 @@
+package com.example.attendance_service.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "projects")
+public class ProjectEntity {
+	
+	@Id
+    @Column(columnDefinition = "uuid")
+    private UUID id;
+
+    @Column(name = "project_name")
+    private String projectName;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	
+
+}
---
+++ backend-java/attendance-service/src/main/java/com/example/attendance_service/repository/AttendanceRepository.java
@@ -0,0 +1,24 @@
+package com.example.attendance_service.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.attendance_service.model.AttendanceEntity;
+
+
+@Repository
+public interface AttendanceRepository extends JpaRepository<AttendanceEntity, UUID> {
+
+    List<AttendanceEntity> findByEmployee_IdOrderByDateAsc(UUID employeeId);
+
+    List<AttendanceEntity> findByProject_IdOrderByDateAsc(UUID projectId);
+
+    List<AttendanceEntity> findByEmployee_IdAndProject_IdOrderByDateAsc(UUID employeeId, UUID projectId);
+    
+
+
+    List<AttendanceEntity> findAllByOrderByDateAsc();
+}
