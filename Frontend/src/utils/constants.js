dfd9bf767aef6cf0c7dd2fceb5a558ddac18d8bc
VanisriMurugan-31 <vanisree.m@zigmaneural.com>
2025-11-08 08:20:23 +0000

    constant

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,18 +1,20 @@
 // Different base URLs per r
-export const API_BASES = {
-  SUPERADMIN: import.meta.env.VITE_SUPERADMIN_URL || "http://localhost:5001",
-  ADMIN: import.meta.env.VITE_ADMIN_URL || "http://localhost:5002",
-  EMPLOYEE: import.meta.env.VITE_EMPLOYEE_URL || "http://localhost:5004",
-  PROJECT:import.meta.env.VITE_Project_URL || "http://localhost:8080/api",
-  ATTENDANCE:import.meta.env.VITE_Attendance_URL || "http://localhost:9091/api",
-};
-
-
 // export const API_BASES = {
-//   // SUPERADMIN: import.meta.env.VITE_SUPERADMIN_URL || "http://20.40.57.43:5001/auth/superadmin",
-//   // ADMIN: import.meta.env.VITE_ADMIN_URL || "http://20.40.57.43:5002/admin",
-//   EMPLOYEE: import.meta.env.VITE_EMPLOYEE_URL || "http://20.40.57.43:5004",
+//   SUPERADMIN: import.meta.env.VITE_SUPERADMIN_URL || "http://localhost:5001",
+//   ADMIN: import.meta.env.VITE_ADMIN_URL || "http://localhost:5002",
+//   EMPLOYEE: import.meta.env.VITE_EMPLOYEE_URL || "http://localhost:5004",
+//   PROJECT:import.meta.env.VITE_Project_URL || "http://localhost:8080/api",
+//   ATTENDANCE:import.meta.env.VITE_Attendance_URL || "http://localhost:9091/api",
 // };
+
+
+export const API_BASES = {
+  SUPERADMIN: import.meta.env.VITE_SUPERADMIN_URL || "https://auth-service-nuob.onrender.com",
+  ADMIN: import.meta.env.VITE_ADMIN_URL || "https://admin-service-vb0h.onrender.com",
+  EMPLOYEE: import.meta.env.VITE_EMPLOYEE_URL || "https://employee-service-5od6.onrender.com",
+  PROJECT:import.meta.env.VITE_Project_URL || "http://20.40.57.43:8081/api",
+  ATTENDANCE:import.meta.env.VITE_Attendance_URL || "http://20.40.57.43:9090/api",
+};
 
 // Auth endpoints
 export const AUTH_API = {
