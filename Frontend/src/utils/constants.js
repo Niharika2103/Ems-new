83b0fc8bac65a1c596f676cb70f807d432cf0442
Niharika2103 <niharika.k@zigmaneural.com>
2025-11-08 12:13:06 +0530

    constant

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -12,6 +12,8 @@ export const API_BASES = {
 //   // SUPERADMIN: import.meta.env.VITE_SUPERADMIN_URL || "http://20.40.57.43:5001/auth/superadmin",
 //   // ADMIN: import.meta.env.VITE_ADMIN_URL || "http://20.40.57.43:5002/admin",
 //   EMPLOYEE: import.meta.env.VITE_EMPLOYEE_URL || "http://20.40.57.43:5004",
+  // PROJECT:import.meta.env.VITE_Project_URL || "http://20.40.57.43:8081/api",
+
 // };
 
 // Auth endpoints
