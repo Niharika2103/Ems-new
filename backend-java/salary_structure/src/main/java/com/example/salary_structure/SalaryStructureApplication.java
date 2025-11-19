451720b981660e438daa8ab3e36a166a83588dfd
VanisriMurugan-31 <vanisree.m@zigmaneural.com>
2025-11-19 09:39:58 +0000

    completed salary structure insert

--- backend-java/salary_structure/src/main/java/com/example/salary_structure/SalaryStructureApplication.java
+++ backend-java/salary_structure/src/main/java/com/example/salary_structure/SalaryStructureApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SalaryStructureApplication {
+public class
+SalaryStructureApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalaryStructureApplication.class, args);
