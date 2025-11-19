22316ba4c89916e6b977073d4b3638bd7c21ca76
VanisriMurugan-31 <vanisree.m@zigmaneural.com>
2025-11-19 05:49:25 +0000

    created salary structure folder

---
+++ backend-java/salary_structure/src/test/java/com/example/salary_structure/SalaryStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salary_structure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryStructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
