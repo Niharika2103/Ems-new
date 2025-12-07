063f556ac46fe08f3563f1ba92d2f884edb29101
VanisriMurugan-31 <vanisree.m@zigmaneural.com>
2025-12-07 15:59:38 +0000

    side bar and probation page only for admin

--- Frontend/src/pages/Probation/probation_management.jsx
+++ Frontend/src/pages/Probation/probation_management.jsx
@@ -92,18 +92,6 @@ const ProbationManagementSystem = () => {
 
             <div className="bg-white rounded-lg shadow-sm p-6">
               <div className="flex items-center justify-between mb-2">
-                <p className="text-gray-600 text-sm">Pending Review</p>
-                <div className="bg-yellow-100 p-2 rounded-lg">
-                  <AlertCircle className="text-yellow-600" size={20} />
-                </div>
-              </div>
-              <p className="text-3xl font-bold text-gray-800">
-                {/* {probationList.filter(e => e.status === 'pending_review').length} */}
-              </p>
-            </div>
-
-            <div className="bg-white rounded-lg shadow-sm p-6">
-              <div className="flex items-center justify-between mb-2">
                 <p className="text-gray-600 text-sm">Ending Soon</p>
                 <div className="bg-red-100 p-2 rounded-lg">
                   <Bell className="text-red-600" size={20} />
