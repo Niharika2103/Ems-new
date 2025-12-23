cbb7920962e331ae086ba8f2499417c3207cb44e
VanisriMurugan-31 <vanisree.m@zigmaneural.com>
2025-12-23 06:15:47 +0000

    updated code

---
+++ backend/services/freelancer-service/src/index.js
@@ -0,0 +1,28 @@
+// src/index.js
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import freelancerRoutes from "./routes/Freelancer.routes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// static access for uploaded files
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+
+// base route for freelancer documents
+app.use("/freelancer", freelancerRoutes);
+
+const PORT = process.env.PORT || 5005;
+app.listen(PORT, () => {
+  console.log(`🚀 Freelancer-service running on port ${PORT}`);
+});
